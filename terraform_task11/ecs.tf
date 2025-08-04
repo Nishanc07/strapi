0e84122380bc9ffa277d039239b41344fa531980
Nisha N C <nishanc1711@gmail.com>
2025-08-04 16:37:25 +0530

    add

--- terraform_task11/ecs.tf
+++ terraform_task11/ecs.tf
@@ -11,14 +11,61 @@ resource "aws_cloudwatch_log_group" "nisha_strapi" {
   retention_in_days = 7
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecsTaskRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+
+   lifecycle {
+    ignore_changes = [
+      description
+    ]
+  }
+}
+
+
+
+
+
+
 resource "aws_ecs_task_definition" "nisha_task" {
   family                   = "nisha-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn      = data.aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+
   container_definitions = jsonencode([
     {
       name      = "strapi"
@@ -30,16 +77,14 @@ resource "aws_ecs_task_definition" "nisha_task" {
           protocol      = "tcp"
         }
       ]
-
       logConfiguration = {
         logDriver = "awslogs",
         options = {
-          awslogs-group         = aws_cloudwatch_log_group.nisha_strapi.name,
-          awslogs-region        = var.region,
+          awslogs-group         = "/ecs/nisha-strapi",
+          awslogs-region        = "us-east-2",
           awslogs-stream-prefix = "ecs"
         }
       }
-
       environment = [
         { name = "DATABASE_CLIENT", value = "postgres" },
         { name = "DATABASE_HOST", value = aws_db_instance.nisha_rds.address },
--- terraform_task11/iam.tf
+++ terraform_task11/iam.tf
@@ -1,31 +1,31 @@
-
-data "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-}
-
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_policy_attachment" {
-  role       = data.aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-
-# CodeDeploy Service Role (created manually or via AWS console)
-data "aws_iam_role" "codedeploy_role" {
+resource "aws_iam_role" "codedeploy_role" {
   name = "CodeDeployServiceRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
 }
 
-# Attach necessary inline policies to the CodeDeploy service role
 resource "aws_iam_role_policy" "codedeploy_inline_policy" {
-  name = "CodeDeployECSPermissions-Nisha"
-  role = data.aws_iam_role.codedeploy_role.id
+  name = "CodeDeployECSPermissions"
+  role = aws_iam_role.codedeploy_role.id
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
-        Action = [
+        Effect   = "Allow",
+        Action   = [
           "ecs:DescribeServices",
           "ecs:UpdateService",
           "ecs:ListTasks",
@@ -50,11 +50,8 @@ resource "aws_iam_role_policy" "codedeploy_inline_policy" {
   })
 }
 
-# Attach the managed AWS policy for CodeDeploy ECS
-resource "aws_iam_role_policy_attachment" "codedeploy_managed_policy" {
-  role       = data.aws_iam_role.codedeploy_role.name
+resource "aws_iam_role_policy_attachment" "codedeploy_ecs_policy" {
+  role       = aws_iam_role.codedeploy_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 }
-
-
 
