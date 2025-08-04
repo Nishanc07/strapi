6cae50203b2ffd1c864633782cb426984f5a23d9
Nisha N C <nishanc1711@gmail.com>
2025-08-04 15:06:18 +0530

    add

--- terraform_task11/iam.tf
+++ terraform_task11/iam.tf
@@ -1,3 +1,25 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_ecs.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_attach" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "assume_ecs" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+
+
 resource "aws_iam_role" "codedeploy_role" {
   name = "CodeDeployServiceRole"
 
